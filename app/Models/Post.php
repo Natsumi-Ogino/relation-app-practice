f4ec77f95fb11de0e5da9f97182309c202ad3789
Natsumi-Ogino <273796069+Natsumi-Ogino@users.noreply.github.com>
2026-07-27 00:30:16 +0900

    Post/Comment/Tagモデルのリレーション定義、Tinkerでテストデータ作成

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'title',
+        'content',
+    ];
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+    
 }
